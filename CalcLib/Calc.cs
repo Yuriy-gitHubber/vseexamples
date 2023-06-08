c55b56ca0235977e24fa13af3e376c3939d5da9b
Yuriy-gitHubber <hayrullin.yura@gmail.com>
2023-06-08 23:29:03 +0500

    Добавили файлик калькулятор

---
+++ CalcLib/Calc.cs
@@ -0,0 +1,9 @@
+﻿namespace CalcLib;
+public static class Calc
+{
+
+    public static Sum (int a, int b ){
+        return a + b;
+    }
+}
+
---
+++ TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int x = 10;
+            int y = x + 20;
+            Console.WriteLine("Всем хай!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
